e1415b2ea243c23f0f59824c43e94b098ccc6042
essohanam <essohanam.alou@imsp-uac.org>
2022-12-26 07:14:51 +0000

    completed linked list daata structure project

--- LinkedList.js
+++ LinkedList.js
@@ -5,7 +5,7 @@ class LinkedList {
         this.HEAD = null
     }
 
-    appendValue(value) {
+    append(value) {
         if (this.HEAD == null) {
             const newNode = new Node(value, null);
             this.HEAD = newNode;
@@ -80,40 +80,141 @@ class LinkedList {
         let listNode = this.HEAD;
         for (let i = 0; i < index; i += 1) {
             if (listNode.next == null) {
-                console.error('index out of bound');
+                // console.error('index out of bound');
                 return;
-            } else {
-                listNode = listNode.next;
             }
+            listNode = listNode.next;
         }
 
         return listNode;
     }
 
     pop() {
-        if (this.HEAD = null) {
+        if (this.size() == 0) {
             console.error('it is an empty list');
             return;
         }
 
-        let listNode = this.HEAD;
-        let listNodeIndex = 0;
-        while (!(listNode.next == null)) {
-            listNode = listNode.next;
-            listNodeIndex += 1;
+        if (this.size() == 1) {
+            this.HEAD = null;
+            return;
         }
 
+        const beforeTailIndex = this.size - 2;
+        this.at(beforeTailIndex).next = null;
+    }
 
+    contains(value) {
+        if (this.HEAD == null) {
+            console.error('it is an empty list');
+            return;
+        }
+
+        if (this.HEAD.value == value) {
+            return true;
+        }
+
+        let currentNode = this.HEAD;
+        while (!(currentNode.next == null)) {
+            currentNode = currentNode.next;
+            if (currentNode.value == value) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    find(value) {
+        if (!this.contains(value)) {
+            return null;
+        }
+
+        let currentNode = this.HEAD;
+        let currentNodeIndex = 0;
+        while (!(currentNode.value == value)) {
+            currentNode = currentNode.next;
+            currentNodeIndex += 1;
+        }
+
+        return currentNodeIndex;
+
+    }
+
+    toString() {
+        if (this.HEAD == null) {
+            return '';
+        }
+
+        let currentNode = this.HEAD;
+        let linkedListAsString = `(${currentNode.value})`;
+        if (currentNode.next == null) {
+            return linkedListAsString;
+        }
+
+        while (!(currentNode.next == null)) {
+            currentNode = currentNode.next;
+            linkedListAsString += `->(${currentNode.value})`;
+        }
+
+        return linkedListAsString + '-> null';
+    }
+
+    insertAt(value, index) {
+        if (index > (this.size()) || index < 0) {
+            console.error('index out of bounds');
+            return;
+        }
+
+        if (index == 0) {
+            this.prepend(value);
+            return;
+        }
+        if (index == this.size()) {
+            this.append(value);
+            return;
+        }
+
+        const currentNodeAtTheIndex = this.at(index);
+        const beforeCurrentNodeAtTheIndex = this.at(index - 1);
+        beforeCurrentNodeAtTheIndex.next = new Node(
+            value
+            , currentNodeAtTheIndex
+        );
+
+    }
+
+    removeAt(index) {
+        if ((index > (this.size - 1)) || (index < 0)) {
+            console.error('index is out of bounds');
+            return;
+        }
+
+        if (index == (this.size - 1)) {
+            this.pop();
+            return;
+        }
+        if (index == 0) {
+            this.HEAD = this.at(1);
+            return;
+        }
+        const beforeCurrentNodeAtTheIndex = this.at(index - 1);
+        const afterCurrentNodeAtTheIndex = this.at(index + 1);
+        beforeCurrentNodeAtTheIndex.next = afterCurrentNodeAtTheIndex;
     }
 
 }
 
 const myLinkedList = new LinkedList();
-myLinkedList.appendValue(15);
-myLinkedList.appendValue(25);
-myLinkedList.appendValue(100);
-myLinkedList.appendValue(150);
-myLinkedList.appendValue(200);
-myLinkedList.prepend(10);
-console.log(myLinkedList.at(0));
+myLinkedList.append(15);
+myLinkedList.append(25);
+// myLinkedList.append(100);
+// myLinkedList.append(150);
+// myLinkedList.append(200);
+// myLinkedList.append('emilie');
+// //myLinkedList.append(200);
+//myLinkedList.insertAt(200, 2);
+myLinkedList.removeAt(0);
+console.log(myLinkedList.toString());
 
